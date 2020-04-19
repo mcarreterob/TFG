390514fd5a4de4e6562fc6925fa3182e9eb13f25
Mario Carretero Berrocal <mariocb94@MacBook-Air-de-Mario.local>
2020-04-19 20:09:14 +0200

    19 abril 20:09

---
+++ app.js
@@ -0,0 +1,4 @@
+//var data = JSON.parse('data.json');
+console.log(document.getElementById("question"));
+document.getElementById("question").innerHTML = data.firstName;
+console.log(data);
---
+++ data.js
@@ -0,0 +1,21 @@
+var data = {
+    "firstName": "John mmmmmmmcmmcmcmoocmoicnwlejcnq elvnlqnvjirjncvq jrncqjpinvqirjn vqpjirbnvpirnv q uebpqvnqpjebvqieub vuqeipjbnvqjeirnbvqpijn vbqjporninvqepn bvpqrjenv qjnbvjpqenbvp ojinbvqpioenv bqobnvqpjnvbqojnbv pqonvjqornv  ronvo`rnbvqpornbvpqorbnvpqjnv    pui  ncpeiubnc",
+    "lastName": "Smith",
+    "age": 25,
+    "address": {
+        "streetAddress": "21 2nd Street",
+        "city": "New York",
+        "state": "NY",
+        "postalCode": 10021
+    },
+    "phoneNumbers": [
+        {
+            "type": "home",
+            "number": "212 555-1234"
+        },
+        {
+            "type": "fax",
+            "number": "646 555-4567"
+        }
+    ]
+  };
