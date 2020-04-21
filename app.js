1d1283b1064662fb2f004aa6781557434912f481
Mario Carretero Berrocal <mariocb94@MacBook-Air-de-Mario.local>
2020-04-21 20:17:01 +0200

    21 abril 20:16

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-//var data = JSON.parse('data.json');
-console.log(document.getElementById("question"));
-document.getElementById("question").innerHTML = data.firstName;
 console.log(data);
+var index = 1;
+function init(){
+  document.getElementById("question").innerHTML = index + ". ";
+  document.getElementById("question").innerHTML += data[index].question;
+}
+
+function insertAnswer(type) {
+  if (type ==="TRUE_FALSE") {
+
+  }
+}
+
+function insertQuestion(data, index) {
+  document.getElementById("question").innerHTML = index + ". ";
+  document.getElementById("question").innerHTML += data[index].question;
+}
--- data.js
+++ data.js
@@ -1,21 +1,12 @@
 var data = {
-    "firstName": "John mmmmmmmcmmcmcmoocmoicnwlejcnq elvnlqnvjirjncvq jrncqjpinvqirjn vqpjirbnvpirnv q uebpqvnqpjebvqieub vuqeipjbnvqjeirnbvqpijn vbqjporninvqepn bvpqrjenv qjnbvjpqenbvp ojinbvqpioenv bqobnvqpjnvbqojnbv pqonvjqornv  ronvo`rnbvqpornbvpqorbnvpqjnv    pui  ncpeiubnc",
-    "lastName": "Smith",
-    "age": 25,
-    "address": {
-        "streetAddress": "21 2nd Street",
-        "city": "New York",
-        "state": "NY",
-        "postalCode": 10021
-    },
-    "phoneNumbers": [
-        {
-            "type": "home",
-            "number": "212 555-1234"
-        },
-        {
-            "type": "fax",
-            "number": "646 555-4567"
-        }
-    ]
+    "1": {
+      "question": "La máquina de impactos se puede utilizar para una variedad de impactos leves o fuertes, mientras que la pelota de caucho para impactos pesados y suaves",
+      "answerType": "TRUE_FALSE",
+      "possibleAnswers": {
+          "a": "Verdadero",
+          "b": "Falso"
+      },
+      "correctAnswer": "Verdadero"
+    }
+
   };
