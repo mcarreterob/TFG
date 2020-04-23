5bda4522054e6294bff776116dc082f22504884f
Mario Carretero Berrocal <mariocb94@MacBook-Air-de-Mario.local>
2020-04-23 20:15:32 +0200

    23/4 20:15

--- app.js
+++ app.js
@@ -14,4 +14,23 @@ function insertAnswer(type) {
 function insertQuestion(data, index) {
   document.getElementById("question").innerHTML = index + ". ";
   document.getElementById("question").innerHTML += data[index].question;
+
 }
+
+function capturar()
+    {
+        // obtenemos e valor por el numero de elemento
+        var porElementos=document.forms["form1"].elements.selected.value;
+        console.log(document.forms["form1"].elements.selected.value);
+        // Obtenemos el valor por el id
+        // var porId=document.getElementById("nombre").value;
+        // // Obtenemos el valor por el Nombre
+        // var porNombre=document.getElementsByName("nombre")[0].value;
+        // // Obtenemos el valor por el tipo de tag
+        // var porTagName=document.getElementsByTagName("input")[0].value;
+        // Obtenemos el valor por el nombre de la clase
+        //var porClassName=document.getElementsByClassName("formulario")[0].value;
+
+        document.getElementById("resultado").innerHTML=" \
+            Por elementos: "+porElementos;
+    }
