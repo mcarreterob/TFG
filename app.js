7cf37f10c9a62c24efdc4e6f320a41f1bdcd6c81
Mario Carretero Berrocal <mariocb94@MacBook-Air-de-Mario.local>
2020-05-02 15:23:14 +0200

    2 mayo 15:23

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ function init(){
 function insertAnswer(index) {
   var type = data[index][index + 1].answerType,
       elementLocation = document.getElementById("dynamicData"),
-      answer;
+      answer,
+      numberAnswers,
+      possAnswer,
+      value;
   switch (type) {
     case "TRUE_FALSE":
       answer = '<p><input type="radio" name="selected" value="Verdadero">  Verdadero</p>'
@@ -19,6 +22,16 @@ function insertAnswer(index) {
     case "OPEN":
       answer = '<p><input type="text" name="selected" style="font-size:1.2vw;width:2vw"> dB</p>';
       elementLocation.insertAdjacentHTML('afterbegin', answer);
+      break;
+    case "MULTI":
+      numberAnswers = data[index][index + 1].numberAnswers;
+      for (var i = 0; i < numberAnswers; i++) {
+        possAnswer = data[index][index + 1].possibleAnswers[i];
+        answer = '<p><input type="radio" name="selected" value=' + possAnswer + '>  ' + possAnswer + '</p>';
+        elementLocation.insertAdjacentHTML('afterbegin', answer);
+      }
+      numberAnswers = null;
+      possAnswer = null;
       break;
     default:
       alert("Ups! Ha habido algún error");
@@ -32,6 +45,7 @@ function insertQuestion(data, index) {
 
 function getNext()
     {
+          console.log(index);
       if(index < data.length - 1) {
         var porElementos=document.forms["form1"].elements.selected.value;
         console.log(document.forms["form1"].elements.selected.value);
@@ -46,7 +60,6 @@ function getNext()
 
 function getPrevious()
     {
-      console.log(index);
       if(index > 0) {
         var porElementos=document.forms["form1"].elements.selected.value;
         console.log(document.forms["form1"].elements.selected.value);
@@ -54,5 +67,8 @@ function getPrevious()
         index --;
         insertQuestion(data, index);
         insertAnswer(index);
+      }
+      if (document.getElementsByTagName('I')[1].className === "fas fa-door-open") {
+        document.getElementById("next").innerHTML = '<i class="fa fa-chevron-right">';
       }
     }
--- data.js
+++ data.js
@@ -5,8 +5,8 @@ var data = [
                   + " mientras que la pelota de caucho para impactos pesados y suaves",
       "answerType": "TRUE_FALSE",
       "possibleAnswers": {
-          "a": "Verdadero",
-          "b": "Falso"
+          "0": "Verdadero",
+          "1": "Falso"
       },
       "correctAnswer": "Verdadero"
     }
@@ -19,6 +19,53 @@ var data = [
       "answerType": "OPEN",
       "correctAnswer": 60
     }
+  },
+  {
+    "3": {
+      "question": "El nivel de ruido de fondo, ¿en qué recinto debe ser medido?",
+      "answerType": "MULTI",
+      "numberAnswers": 2,
+      "possibleAnswers": {
+          "0": "Receptor",
+          "1": "Emisor"
+      },
+      "correctAnswer": "Receptor"
+    }
+  },
+  {
+    "4": {
+      "question": "El área de absorción equivalente se puede calcular utilizando la fórmula del TR de Sabine"
+                  + " (T = 0.161V/A) si y sólo si es el único elemento absorbente en el recinto.",
+      "answerType": "TRUE_FALSE",
+      "possibleAnswers": {
+          "0": "Verdadero",
+          "1": "Falso"
+      },
+      "correctAnswer": "Verdadero"
+    }
+  },
+  {
+    "5": {
+      "question": "Las mediciones que deben hacerse incluyen:",
+      "answerType": "MULTI",
+      "numberAnswers": 4,
+      "possibleAnswers": {
+          "0": "Los niveles de presión acútsica en el recinto receptor con la fuente de impactos funcionando,"
+                + " los niveles de ruido de fondo en el recinto receptor cuando la fuente de impactos está "
+                + "apagada y los TR en el recinto emisor y en el recinto receptor",
+          "1": "Los niveles de presión acútsica en el recinto receptor con la fuente de impactos funcionando,"
+                + " los niveles de ruido de fondo en el recinto emisor y en el recinto receptor cuando la fuente de impactos está "
+                + "apagada y los TR en el recinto emisor y en el recinto receptor",
+          "2": "Los niveles de presión acútsica en el recinto receptor con la fuente de impactos funcionando,"
+                + " los niveles de ruido de fondo en el recinto emisor y en el recinto receptor cuando la fuente de impactos está "
+                + "apagada y los TR en el recinto emisor",
+          "3": "Los niveles de presión acútsica en el recinto receptor con la fuente de impactos funcionando,"
+                + " los niveles de ruido de fondo en el recinto receptor cuando la fuente de impactos está "
+                + "apagada y los TR en el recinto receptor"
+      },
+      "correctAnswer": "Los niveles de presión acútsica en el recinto receptor con la fuente de impactos funcionando,"
+                        + " los niveles de ruido de fondo en el recinto receptor cuando la fuente de impactos está "
+                        + "apagada y los TR en el recinto receptor"
+    }
   }
-
   ];
